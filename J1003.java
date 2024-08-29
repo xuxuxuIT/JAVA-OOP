2974922dc958b44edcdf26a600eb3c801d2528ba
xuxuxuIT <nhulinh244@gmail.com>
2024-08-29 20:43:51 +0700

    Add files via upload

--- J1003.java
+++ J1003.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package test;
 
-/**
- *
- * @author admin
- */
+import java.util.Scanner;
+
 public class J1003 {
-    
+    public static void main(String[] args) {
+    Scanner sc= new Scanner ( System.in);
+        float a= sc.nextFloat(), b= sc.nextFloat();
+        if ( a==0 && b==0){
+            System.out.println("VSN");
+        }else if ( a==0 && b!=0){
+            System.out.println("VN");
+        }else{
+            float x= (-b/a);
+            System.out.printf("%.2f",x);
+        }
+    } 
 }
