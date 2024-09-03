f16d915a016a7d7174f19c553a46c6c6c5c90fa5
xuxuxuIT <nhulinh244@gmail.com>
2024-09-04 02:20:47 +0700

    Add files via upload

---
+++ Giaocuahaimang.java
@@ -0,0 +1,29 @@
+import java.util.*;
+// in theo gia tri tang dan
+public class Giaocuahaimang {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt();
+        int []a= new int [n];
+        int []b= new int[m];
+        int []d1= new int[1005];
+        int[]d2= new int[1005];
+        for( int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            d1[a[i]]++;
+        }
+        for( int i=0;i<m;i++){
+            b[i]=sc.nextInt();
+            d2[b[i]]++;
+        }
+        ArrayList<Integer> giao= new ArrayList<>();
+        for(int val=0;val<1005;val++){
+            if(d1[val]>0 && d2[val]>0){
+                giao.add(val);
+            }
+        }
+        for( int x:giao){
+            System.out.print(x+" ");
+        }
+    }
+}
