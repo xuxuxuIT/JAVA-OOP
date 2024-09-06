a0b73adc8194daaba48f92afaf72e4edcd734d87
xuxuxuIT <nhulinh244@gmail.com>
2024-09-06 12:49:18 +0700

    Add files via upload

---
+++ J3013_Hieusonguyenlon1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.math.BigInteger;
+
+public class J3013_Hieusonguyenlon1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            BigInteger a= sc.nextBigInteger();
+            BigInteger b= sc.nextBigInteger();
+            BigInteger c= a.subtract(b).abs();
+            StringBuilder sb= new StringBuilder(c.toString());
+            int max_val= Math.max(a.toString().length(),b.toString().length());
+            while(sb.length()<max_val){
+                sb.insert(0,'0');
+            }
+            System.out.println(sb.toString());
+        }       
+    }   
+}
